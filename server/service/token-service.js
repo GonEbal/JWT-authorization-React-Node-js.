48477141e85936e873806b301351753f44f87846
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-06-27 12:48:59 -0700

    changes to getData (for only authorized users)

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -13,7 +13,7 @@ class TokenService {
 
     validateAccessToken(token) {
         try {
-            const userData = jwt.verify(token, proccess.env.JWT_ACCESS_SECRET);
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
             return userData;
         } catch (e) {
             return null;
@@ -22,7 +22,7 @@ class TokenService {
 
     validateRefreshToken(token) {
         try {
-            const userData = jwt.verify(token, proccess.env.JWT_REFRESH_SECRET);
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
             return userData;
         } catch (e) {
             return null;
